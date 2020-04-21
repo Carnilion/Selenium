b1d97080143eb3b92fff97f6b8beef5650811c1a
mzyrkowski <mzyrkowski@griddynamics.com>
2020-04-21 11:08:02 +0200

    cleaned pom

--- src/test/java/com/company/SeleniumTest.java
+++ src/test/java/com/company/SeleniumTest.java
@@ -9,16 +9,19 @@ public class SeleniumTest {
 
     @Test
     public void startWebdriver() {
-
-
 //        System.setProperty("webdriver.chrome.driver", "/Users/mzyrkowski/selenium_chrome/chromedriver");
 //        WebDriver driver = new ChromeDriver();
 
+//        System.setProperty("webdriver.gecko.driver", "/Users/mzyrkowski/selenium/geckodriver");
+//        WebDriver driver = new FirefoxDriver();
+//
+//        driver.get("http://www.learn-automation.com");
+//        System.out.println(driver.getTitle());
+
         System.setProperty("webdriver.gecko.driver", "/Users/mzyrkowski/selenium/geckodriver");
         WebDriver driver = new FirefoxDriver();
-
-        driver.get("http://www.learn-automation.com");
+        driver.navigate().to("http://www.learn-automation.com");
         System.out.println(driver.getTitle());
-
+        driver.close();
     }
 }
