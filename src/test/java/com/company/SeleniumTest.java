3570ca492c3523d6aa37afdb9f0b376a86d2631d
mzyrkowski <mzyrkowski@griddynamics.com>
2020-04-21 09:56:04 +0200

    init tmoBranch

---
+++ src/test/java/com/company/SeleniumTest.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleniumTest {
+
+    @Test
+    public void startWebdriver() {
+
+
+//        System.setProperty("webdriver.chrome.driver", "/Users/mzyrkowski/selenium_chrome/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+
+        System.setProperty("webdriver.gecko.driver", "/Users/mzyrkowski/selenium/geckodriver");
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("http://www.learn-automation.com");
+        System.out.println(driver.getTitle());
+
+    }
+}
